1363e83b03189defe5c6179b520fdba62bd83669
Josh Lee <jleedev@gmail.com>
2019-04-17 19:01:56 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 mod api_response;
-mod cors;
 
-use crate::{api_response::ApiResponse, cors::Cors};
+use crate::api_response::ApiResponse;
 
-use rocket::{get, routes};
+use rocket::{fairing::AdHoc, get, routes};
 use serde_derive::Serialize;
 use std::time::SystemTime;
 
@@ -24,9 +23,17 @@ fn get_time() -> ApiResponse<Timestamp> {
     ApiResponse(out)
 }
 
+/// Unconditionally allow origin * on all responses
+fn cors() -> AdHoc {
+    use rocket::http::hyper::header::*;
+    AdHoc::on_response("CORS Header", |_req, resp| {
+        resp.set_header(AccessControlAllowOrigin::Any);
+    })
+}
+
 fn main() {
     rocket::ignite()
         .mount("/", routes![get_time])
-        .attach(Cors)
+        .attach(cors())
         .launch();
 }
